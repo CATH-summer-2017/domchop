69196f1fe49ea0c49ff987a0115d208eaf66082a
shouldsee <shouldsee.gem@gmail.com>
2017-07-07 16:25:56 +0100

    added_regex_urls

--- doped_cath/tst/urls.py
+++ doped_cath/tst/urls.py
@@ -1,8 +1,42 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
 
 from . import views
 
 urlpatterns = [
+    # ex: /tst/
     url(r'^$', views.index, name='index'),
+
+	# ex: /tst/hello    
     url(r'^hello$', views.index, name='hello'),
+
+    # ex: /tst/5/
+    url(r'^(?P<question_id>[0-9]+)/$', 
+    	views.detail,
+    	name='detail'),
+    
+    # ex: /tst/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$',
+    	views.results,
+    	name = 'results'),
+    
+    # ex:/tst/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$',
+    	views.vote,
+    	name = 'vote'),
+
+
+    # ex:/tst/
+    url(r'^domain/$',
+    	views.domain_collection,
+    	name = 'domain_collection'),
+
+    # ex:/tst/d1fpzD00/
+    url(r'^domain/(?P<domain_id>[\d,a-z,A-Z]+)/$',
+    	views.domain_detail,
+    	name = 'domain_detail'),
+
+	# ex:/tst/superfamily/3.90.190.10/
+	url(r'^superfamily/(?P<homsf_id>[\d,\.]+)/$',
+		views.homsf_s35_collection,
+		name = 'homsf_s35_collection'),
 ]
--- doped_cath/tst/views.py
+++ doped_cath/tst/views.py
@@ -9,5 +9,32 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+from .models import *
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	output = ',</br> '.join([q.question_text for q in latest_question_list])
+	# return HttpResponse("Hello, world. You're at the polls index.")
+	return HttpResponse( '<!DOCTYPE HTML><html>' + ' The output is: <br/>'+ output+'</html>')
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
+####
+def domain_detail(request, domain_id):
+    return HttpResponse("You're viewing detail page on CATH domian %s." % domain_id)
+
+def domain_collection(request):
+    return HttpResponse("You're viewing the whole collection of CATH domains" )
+
+def homsf_s35_collection(request, homsf_id):
+    return HttpResponse("You're viewing the s35 representative structures of homology family %s" % homsf_id)
