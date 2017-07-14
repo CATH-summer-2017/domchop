f9b01b5f36fdc648b9d28eb6b418d24e38e7b9fc
shouldsee <shouldsee.gem@gmail.com>
2017-07-14 12:46:22 +0100

    Refactor table views. Pass "orders" list and "cols" list to allow rapid prototyping of tables

--- doped_cath/tst/views.py
+++ doped_cath/tst/views.py
@@ -9,11 +9,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+
+from django.db.models import Avg,Count,Q
 from .models import *
 
 from CATH_API.lib import *
 import urllib
 import re
+import random
 
 def index(request):
 
@@ -32,58 +35,58 @@ def vote(request, question_id):
 def domain_detail(request, domain_id):
     return HttpResponse("You're viewing detail page on CATH domian %s." % domain_id)
 
-def view_domain_list(request, domain_list):
+def view3d(request):
+	did = "2c7wA00";
+	did = "1fzpD00";
+	context={ "d" :domain.objects.get(domain_id=did)};
+	return render(request,
+			 'tst/3dviewer.html',
+			  context)
 
-	template = loader.get_template('tst/index.html')
-	# for 
+def view_domain_list(request, query_set, orders = None, cols = None):
+
 	sf_list = CATH_superfamily('v4_1_0')[1]
 
-	for q in domain_list:
-		q.s35cnt = 'not found'
-		q_sf =  q.superfamily();
-		for sf in sf_list:
-			if 	sf['cath_id'] == q_sf:
-				q.s35cnt = sf['child_count_s35_code'];
-				break
-	# domain_list = domain_list.all().order_by('-nDOPE','-s35cnt')
-	context = {'domain_list':domain_list,'tst_a':0,
-				}
+	if query_set.model == domain:
+		query_set = query_set.annotate(sf_s35cnt=Count('classification__parent__classification'))	
+		orders = orders or ['-sf_s35cnt','-nDOPE'];
+		query_set = query_set.order_by(*orders)
+		cols=cols or ['domain_id_urled','superfamily_urled','view_chopped','sf_s35cnt','domain_length','nDOPE'];
+
+	if query_set.model == classification:
+		# query_set = query_set.annotate(sf_s35cnt=Count('classification__parent__classification'))	
+		# orders = orders or ['-s35_count','-nDOPE_avg'];
+		query_set = query_set.order_by(*orders)
+		# cols=cols or ['superfamily','s35_count','rep_s35','domain_length','nDOPE'];
+
+		cols=cols or ['superfamily_urled','s35_count','nDOPE_avg','nDOPE_std'];
+
+
+	context = {
+		'query_set':query_set,
+		'tst_a':0,	
+		's35cnts':[],
+		'field_names':cols}
 	return render(request,
-				 'tst/index.html',
+				 # 'tst/index.html',
+				 'tst/view_table.html',
 				  context)
 
 def domain_collection(request):
 	# latest_question_list = Question.objects.order_by('-pub_date')[:5]
 	dquery = request.GET.get('dquery', [])
 
-	# dquery = ['1000','1tf6D01']
 	if dquery:
-		dquery = re.sub('[^A-Za-z0-9\,]+', '', dquery).split(',');
-		domain_list = domain.objects.filter(domain_id__in=dquery)
-		# domain_list = domain_list.order_by('classification__Class','-nDOPE')
-
+		dquery = re.sub('[^A-Za-z0-9\_]+', '', dquery).split('_');
+		# domain_list = domain.objects.filter(domain_id__in=dquery)
+		domain_list = domain.objects.none();
+		for q in dquery:
+			if q:
+				domain_list = domain_list | domain.objects.filter(domain_id__startswith=q[:4]); 
 	else:
-		# domain_list
 		domain_list = domain.objects.filter(nDOPE__gte=1.5);
 
-		# s35cnt_lst=[]
-		sf_list = CATH_superfamily('v4_1_0')[1]
-
-		s = domain_list.all()
-		for q in s:
-				q.s35cnt = 'not found'
-				q_sf =  q.superfamily();
-				for sf in sf_list:
-					if 	sf['cath_id'] == q_sf:
-						q.s35cnt = sf['child_count_s35_code'];
-						# s35cnt_lst.append(sf['child_count_s35_code']);
-						break
-		# dope_lst = domain_list.values_list('nDOPE');
-		# s = domain_list.all()
-		s = sorted(s, key = lambda x: (-int(x.s35cnt), - x.nDOPE))
-		domain_list = s;
-		# domain_list = domain_list.all().order_by('-s35cnt','-nDOPE',)
-		# domain_list = domain.objects.filter(resolution__lte=1.0);
+	orders = ['-sf_s35cnt','-nDOPE']
 
 	# domain_list = 
 
@@ -91,21 +94,33 @@ def domain_collection(request):
 	# tst_a = dquery
 	'tst/domain/?dquery=1gjjA00&dquery=4567Cud'
 
-	return view_domain_list(request,domain_list)
+	return view_domain_list(request,domain_list,orders)
 
 
-def homsf_s35_collection(request, homsf_id):
-	lst = (int(x) for x in homsf_id.split('.'))
-	homsf = classification.objects.filter(Class=next(lst,None),
-								arch=next(lst,None),
-								topo=next(lst,None),
-								homsf=next(lst,None),
-								s35=next(lst,0),
-								# s35=None
-								)[0]
+def homsf_s35_collection(request, homsf_id = None):
+	if not homsf_id:
+		### filter for the superfamily/ index page
+		# homsf_list = (classification.homsf_objects.filter(nDOPE_avg__gte=1.0 ) | 
+		# 	classification.homsf_objects.filter(nDOPE_std__gte = 0.1 ))
+		homsf_list = classification.homsf_objects.filter(s35_count__gte=1000)
 
-	domain_list = domain.objects.filter(classification__in=homsf.classification_set.all())
-	domain_list = domain_list.order_by('-nDOPE')
-	return view_domain_list(request,domain_list)
+		return view_domain_list(request,homsf_list,
+				orders = ['-nDOPE_avg'],
+				cols = ['superfamily_urled','s35_count','nDOPE_avg','nDOPE_std'])
+	else:
+		lst = (int(x) for x in homsf_id.split('.'))
+		homsf = classification.objects.filter(Class=next(lst,None),
+									arch=next(lst,None),
+									topo=next(lst,None),
+									homsf=next(lst,None),
+									s35=next(lst,0),
+									# s35=None
+									)[0]
+
+		domain_list = domain.objects.filter(classification__in=homsf.classification_set.all())
+		domain_list = domain_list.order_by('-nDOPE')
+		# cols = ['domain_id','superfamily_urled','sf_s35cnt','domain_length','nDOPE'];
+		cols = [];
+		return view_domain_list(request,domain_list,cols=cols)
     
     # return HttpResponse("You're viewing the s35 representative structures of homology family %s" % homsf_id)
